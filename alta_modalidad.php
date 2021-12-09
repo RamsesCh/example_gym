d0d0aa43fe064ed04a874eec969a12a2dee32a0d
jchavarria <ramses.chav@gmail.com>
2021-12-09 01:56:13 -0600

    fix

--- alta_modalidad.php
+++ alta_modalidad.php
@@ -6,7 +6,8 @@ $modalidad = $_POST['modalidad'];
 $costo = $_POST['costo'];
 $status = $_POST['status'];
 
-$insertar = $mysqli->query("INSERT INTO modalidad VALUES ('','$modalidad', '$costo', '$status')");
+//$insertar = $mysqli->query("INSERT INTO modalidad VALUES ('','$modalidad', '$costo', '$status')");
+$insertar = $mysqli->query("INSERT INTO modalidad (modalidad.id_modalidad, modalidad.tipo_modalidad, modalidad.costo, modalidad.status ) VALUES (NULL,'$modalidad', '$costo', '$status')");
 
 echo '<script>alert("Modalidad dada de alta")</script>';
 echo "<script>location.href='perfil_admin.php'</script>";
