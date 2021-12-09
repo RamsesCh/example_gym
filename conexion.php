eba2d814f009cb61ca36732cb238f96cf7ba869a
jchavarria <ramses.chav@gmail.com>
2021-12-08 20:41:40 -0600

    fix pass database

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php 
-$mysqli=new mysqli ("localhost", "root", "", "practica4");
+$mysqli=new mysqli ("localhost", "root", "password", "practica4");
 	if($mysqli->connect_errno){
 		echo "fallo al conectar: (".$mysqli->connect_errno.")".$mysqli
 		->connect_error;
